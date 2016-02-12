6c57fb4d378a1b1565c04c63dca4eec92ea16a47
Hispar <hispar@gmail.com>
2016-02-12 23:15:05 +0100

    Solving date errors

--- assets/app/main.js
+++ assets/app/main.js
@@ -9,8 +9,10 @@
         $http.defaults.xsrfHeaderName = 'X-CSRFToken';
 
         this.loadShips = function () {
+            this.tab = 1;
             $http.get('/api/v1/ships/')
                 .then(function (res) {
+                    res.data.reservation_date = new Date(res.data.reservation_date);
                     $scope.ships = res.data.results;
                 });
         };
@@ -22,6 +24,7 @@
         this.selectShip = function (id) {
             $http.get('/api/v1/ships/' + id)
                 .then(function (res) {
+                    res.data.reservation_date = new Date(res.data.reservation_date);
                     $scope.ship = res.data;
                 });
             this.tab = 2;
@@ -34,6 +37,7 @@
         this.editShip = function (id) {
             $http.get('/api/v1/ships/' + id)
                 .then(function (res) {
+                    res.data.reservation_date = new Date(res.data.reservation_date);
                     $scope.ship = res.data;
                 });
             this.tab = 3;
@@ -44,10 +48,13 @@
          * @param ship
          */
         this.reserve = function (ship) {
+            //ship.reservation_date = ship.reservation_date.getFullYear() + '-' + ship.reservation_date.getMonth() + '-' + ship.reservation_date.getDate();
             $http.put('/api/v1/ships/' + ship.id + '/', ship)
                 .then(function (res) {
+                    res.data.reservation_date = new Date(res.data.reservation_date);
                     $scope.ship = res.data;
                 });
+            ship.reservation_date = new Date(ship.reservation_date);
         };
 
         /**
@@ -77,5 +84,27 @@
         this.isSelected = function (checkTab) {
             return this.tab === checkTab;
         };
+
+        /**
+         * Load map
+         */
+        this.showmap = function (lat, lon) {
+            var mapOptions = {
+                center: new google.maps.LatLng(28.1823294, -82.352912),
+                zoom: 9,
+                mapTypeId: google.maps.MapTypeId.HYBRID,
+                scrollwheel: false,
+                draggable: false,
+                panControl: true,
+                zoomControl: true,
+                mapTypeControl: true,
+                scaleControl: true,
+                streetViewControl: true,
+                overviewMapControl: true,
+                rotateControl: true,
+            };
+            var mapDiv = document.getElementById('map');
+            var map = new google.maps.Map(mapDiv, mapOptions);
+        }
     });
 })();
