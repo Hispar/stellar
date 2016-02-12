1dfbf943b1db4372a2496df16ee4c8adce43bd75
Hispar <hispar@gmail.com>
2016-02-12 23:13:17 +0100

    Remove authentication from api

--- stellar/settings/base.py
+++ stellar/settings/base.py
@@ -223,9 +223,9 @@ WSGI_APPLICATION = '%s.wsgi.application' % SITE_NAME
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ],
+    # 'DEFAULT_PERMISSION_CLASSES': [
+    #     'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    # ],
     # 'DEFAULT_RENDERER_CLASSES': [
     #     'rest_framework.renderers.JSONRenderer',
     # ],
